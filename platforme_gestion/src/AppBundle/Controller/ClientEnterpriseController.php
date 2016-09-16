acf7c9e176428a315321af9cdee524b10a9100d1
mmasure <masure.marc.59@gmail.com>
2016-09-16 12:33:20 +0200

    update controller client with variable array update project_dash.html for POC

--- platforme_gestion/src/AppBundle/Controller/ClientEnterpriseController.php
+++ platforme_gestion/src/AppBundle/Controller/ClientEnterpriseController.php
@@ -48,7 +48,79 @@ class ClientEnterpriseController extends Controller {
 	 */
 	function ProjectDashboard(){
 		return $this->render('project/project_dash.html.twig', array(
-                    'type_connection' => 'client'
+                    'type_connection' => 'client',
+                    'descriptif_projet' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris molestie, lorem in dapibus tincidunt, tellus turpis lobortis est, vel tristique arcu ipsum vitae tortor. Pellentesque laoreet congue felis, at vehicula massa volutpat sit amet. Aliquam erat volutpat. Nulla eu tristique velit. Vestibulum feugiat in eros a pulvinar. Donec nibh dolor, ullamcorper at turpis vitae, aliquet ultrices tellus. Sed hendrerit et dui a luctus. Ut felis sapien, interdum eu sollicitudin at, feugiat id eros. Praesent in dolor ante. Aliquam sodales imperdiet porttitor.',
+                    'personns' => array(
+                    		'0' => array(
+                    			'nom' => 'Jean',
+                    			'prenom' => 'Darc',
+                    			'post' => 'Chef de projet'),
+                    		'1' => array(
+                    			'nom' => 'Duck',
+                    			'prenom' => 'Daffy',
+                    			'post' => 'Commercial'),
+                    		'2' => array(
+                    			'nom' => 'Duck',
+                    			'prenom' => 'Donald',
+                    			'post' => 'Chef technique'),
+                    		'3' => array(
+                    			'nom' => 'Porc',
+                    			'prenom' => 'Cinet',
+                    			'post' => 'Developpeur'),
+                    		'4' => array(
+                    			'nom' => 'Mickey',
+                    			'prenom' => 'Lesourisso',
+                    			'post' => 'Developpeur'),
+                    		),
+                    'bugs' => array(
+                    		'0' => array(
+                    			'title' => 'test0',
+                    			'descriptif' => 'dsdqsdsqdsqdsqdqs',
+                    			'datecreate' => '00/00/2000',
+                    			'progress' => '10%'),
+                    		'1' => array(
+                    			'title' => 'test1',
+                    			'descriptif' => 'fdfsdfdsfsdfsdfsd',
+                    			'datecreate' => '01/01/2001',
+                    			'progress' => '20%'),
+                    		'2' => array(
+                    			'title' => 'test2',
+                    			'descriptif' => 'dsqdsqdsqdsqfdgdgd',
+                    			'datecreate' => '03/03/2003',
+                    			'progress' => '30%'),
+                    		'3' => array(
+                    			'title' => 'test3',
+                    			'descriptif' => 'dsdqsdsqffdfd',
+                    			'datecreate' => '04/04/2004',
+                    			'progress' => '40%'),
+                    		'4' => array(
+                    			'title' => 'test4',
+                    			'descriptif' => 'dsqfggdsgqfdgfdgfdsgsfdgdfs',
+                    			'datecreate' => '05/05/2005',
+                    			'progress' => '50%'),
+                    		),
+                    'docs' => array(
+                    		'0' => array(
+                    			'title' => 'test0',
+                    			'descriptif' => 'http://wikiwiki.fr/hello',
+                    			'datecreate' => '00/00/2000'),
+                    		'1' => array(
+                    			'title' => 'test1',
+                    			'descriptif' => 'http://wikiwiki.fr/hello',
+                    			'datecreate' => '01/01/2001'),
+                    		'2' => array(
+                    			'title' => 'test2',
+                    			'descriptif' => 'http://wikiwiki.fr/hello',
+                    			'datecreate' => '03/03/2003'),
+                    		'3' => array(
+                    			'title' => 'test3',
+                    			'descriptif' => 'http://wikiwiki.fr/hello',
+                    			'datecreate' => '04/04/2004'),
+                    		'4' => array(
+                    			'title' => 'test4',
+                    			'descriptif' => 'http://wikiwiki.fr/hello',
+                    			'datecreate' => '05/05/2005'),
+                    		)
         ));
 	}
 	/**
